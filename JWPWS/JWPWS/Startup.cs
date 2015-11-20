e702e8f89e150fcdfa941621d3685678da7ac52a
jacweave <jacweave@indiana.edu>
2015-11-20 14:56:11 -0500

    Solution init;

---
+++ JWPWS/JWPWS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JWPWS.Startup))]
+namespace JWPWS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
